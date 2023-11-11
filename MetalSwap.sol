84c3fc9027b511f44d9ff3c2dec84a2f709539e6
DEFI FOUNDATION <106986644+DEFI-Foundation@users.noreply.github.com>
2023-11-11 17:48:07 +0100

    add xmt linea contract

---
+++ MetalSwap.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.3/access/AccessControl.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MetalSwap is ERC20, ERC20Burnable, AccessControl, ERC20Permit {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(address defaultAdmin, address minter)
+        ERC20("MetalSwap", "XMT")
+        ERC20Permit("MetalSwap")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}
